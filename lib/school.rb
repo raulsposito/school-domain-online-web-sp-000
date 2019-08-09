f31f6cba90a7c1a6aa71658ca5661bee5ebb8462
Raul Sposito <raulsposito@gmail.com>
2019-08-09 12:57:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ class School
   
   def add_student(name, grade)
   roster[grade] ||= []
-  roster[grade] << student
+  roster[grade] << name
+  end
 roster[level] ||= []
     roster[level] << student
   end 
